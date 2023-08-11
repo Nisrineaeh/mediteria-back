638a680062548dfc5f3d51fa685e106eb408d2af
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-11 10:13:00 +0200

    install class-validator pr create utilisateur

--- src/utilisateur/dto/create-utilisateur.dto.ts
+++ src/utilisateur/dto/create-utilisateur.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUtilisateurDto {}
+import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUtilisateurDto {
+    @IsNotEmpty()
+    nom: string;
+
+    @IsNotEmpty()
+    prenom: string;
+
+    @IsNotEmpty()
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    mot_de_passe: string;
+}
+
+
