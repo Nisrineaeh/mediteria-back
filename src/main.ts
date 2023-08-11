0cad272760bd5e94da3a57b6b3654b4ed11ff4cf
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-11 10:34:39 +0200

    CorsOptions ok

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+dotenv.config({ path: '.env.local' });
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  }
+
+
+  app.enableCors(corsOptions);
+
+
+  await app.listen(8080);
+
 }
 bootstrap();
