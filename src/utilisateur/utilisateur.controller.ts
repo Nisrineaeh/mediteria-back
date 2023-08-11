00bc0e209e8c554fea74cf66195f0c0961a2d6c5
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-11 10:21:27 +0200

    utilisateur controller + service ok

--- src/utilisateur/utilisateur.controller.ts
+++ src/utilisateur/utilisateur.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UtilisateurService } from './utilisateur.service';
 import { CreateUtilisateurDto } from './dto/create-utilisateur.dto';
 import { UpdateUtilisateurDto } from './dto/update-utilisateur.dto';
+import { Utilisateur } from './entities/utilisateur.entity';
 
 @Controller('utilisateur')
 export class UtilisateurController {
-  constructor(private readonly utilisateurService: UtilisateurService) {}
+  constructor(private readonly utilisateursService: UtilisateurService) { }
 
   @Post()
-  create(@Body() createUtilisateurDto: CreateUtilisateurDto) {
-    return this.utilisateurService.create(createUtilisateurDto);
+  async create(@Body() createUtilisateurDto: CreateUtilisateurDto): Promise<Utilisateur> {
+    return this.utilisateursService.create(createUtilisateurDto);
   }
 
   @Get()
-  findAll() {
-    return this.utilisateurService.findAll();
+  async findAll(): Promise<Utilisateur[]> {
+    return this.utilisateursService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.utilisateurService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<Utilisateur> {
+    return this.utilisateursService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUtilisateurDto: UpdateUtilisateurDto) {
-    return this.utilisateurService.update(+id, updateUtilisateurDto);
+  async update(@Param('id') id: string, @Body() updateUtilisateurDto: UpdateUtilisateurDto): Promise<Utilisateur> {
+    return this.utilisateursService.update(+id, updateUtilisateurDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.utilisateurService.remove(+id);
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.utilisateursService.remove(id);
   }
 }
--- src/utilisateur/utilisateur.service.ts
+++ src/utilisateur/utilisateur.service.ts
@@ -1,26 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUtilisateurDto } from './dto/create-utilisateur.dto';
 import { UpdateUtilisateurDto } from './dto/update-utilisateur.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Utilisateur } from './entities/utilisateur.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UtilisateurService {
-  create(createUtilisateurDto: CreateUtilisateurDto) {
-    return 'This action adds a new utilisateur';
+  constructor(
+    @InjectRepository(Utilisateur)
+    private readonly utilisateursRepository: Repository<Utilisateur>,
+  ) { }
+
+  async create(createUtilisateurDto: CreateUtilisateurDto): Promise<Utilisateur> {
+    const { mot_de_passe, ...rest } = createUtilisateurDto;
+
+    // Hachage du mot de passe
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(createUtilisateurDto.mot_de_passe, saltRounds);
+
+    // Création de l'utilisateur avec le mot de passe haché
+    const nouvelUtilisateur = this.utilisateursRepository.create({
+      ...rest,
+      mot_de_passe: hashedPassword,
+    });
+
+    return this.utilisateursRepository.save(nouvelUtilisateur);
   }
 
-  findAll() {
-    return `This action returns all utilisateur`;
+  async findAll(): Promise<Utilisateur[]> {
+    return await this.utilisateursRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} utilisateur`;
+  async findOne(id: number): Promise<Utilisateur> {
+    const user = await this.utilisateursRepository.findOne({ where: { id: id } });
+    if (!user) throw new NotFoundException(`L'utilisateur ID n° ${id} n'existe pas !`);
+    return user;
   }
 
-  update(id: number, updateUtilisateurDto: UpdateUtilisateurDto) {
-    return `This action updates a #${id} utilisateur`;
+  async update(id: number, updateUtilisateurDto: UpdateUtilisateurDto): Promise<Utilisateur> {
+    const userToUpDate = await this.findOne(id);
+    Object.assign(userToUpDate, updateUtilisateurDto);
+    return await this.utilisateursRepository.save(userToUpDate);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} utilisateur`;
+
+
+  async remove(id: any): Promise<void> {
+    try {
+      const user = await this.utilisateursRepository.findOne({ where: { id: id } });
+      if (!user) {
+        throw new NotFoundException(`L'utilisateur avec l'ID ${id} n'a pas été trouvé.`);
+      }
+      await this.utilisateursRepository.remove(user);
+      console.log('Utilisateur supprimé avec succès :', user);
+    } catch (error) {
+      console.error('Erreur lors de la suppression de l\'utilisateur :', error);
+      throw new InternalServerErrorException('Une erreur est survenue lors de la suppression de l\'utilisateur.');
+    }
+  }
+
+  async findByEmailandPassword(email: string, mot_de_passe: string): Promise<Utilisateur | undefined> {
+    console.log('Recherche de l\'utilisateur par e-mail et mot de passe...');
+    const utilisateur = await this.utilisateursRepository.findOne({ where: { email: email } });
+    if (utilisateur) {
+      const match = await bcrypt.compare(mot_de_passe, utilisateur.mot_de_passe);
+      if (match) {
+        return utilisateur;
+      }
+    }
+
+    return null; // Retourne null si l'utilisateur n'est pas trouvé ou si le mot de passe ne correspond pas.
+  }
+
+  async verifierConnexion(email: string, mdp: string): Promise<boolean> {
+    const utilisateur = await this.utilisateursRepository.findOne({ where: { email: email } });
+    if (utilisateur) {
+      const match = await bcrypt.compare(mdp, utilisateur.mot_de_passe);
+      return match;
+    }
+    return false; // Aucun utilisateur trouvé
+  }
+
+  async findByEmail(email: string): Promise<Utilisateur | undefined> {
+    return this.utilisateursRepository.findOne({ where: { email: email } })
   }
 }
