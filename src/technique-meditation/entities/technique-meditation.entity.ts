033bfebaca33b8cdd730719852a99d3c30c26488
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-11 10:09:29 +0200

    creation des entites des tables

--- src/technique-meditation/entities/technique-meditation.entity.ts
+++ src/technique-meditation/entities/technique-meditation.entity.ts
@@ -1,1 +1,32 @@
-export class TechniqueMeditation {}
+import { Column, Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+
+@Entity('technique_meditation')
+export class TechniqueMeditation {
+
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 200, nullable: true })
+    name: string;
+
+    @Column({ length: 200, nullable: true })
+    lien_image: string;
+
+    @Column({ length: 200, unique: true, nullable: true })
+    ambiance: string;
+
+    @Column({ length: 200, unique: true, nullable: true })
+    description: string;
+
+    @Column({ nullable: true })
+    duration: number;
+
+    @Column({ length: 200, nullable: true })
+    lien_audio: string;
+
+    @Column({ length: 200, nullable: true })
+    ressenti: string;
+
+
+} 
--- src/utilisateur/entities/utilisateur.entity.ts
+++ src/utilisateur/entities/utilisateur.entity.ts
@@ -1,1 +1,31 @@
-export class Utilisateur {}
+import { TechniqueMeditation } from 'src/technique-meditation/entities/technique-meditation.entity';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+
+@Entity('utilisateurs')
+export class Utilisateur {
+
+    @PrimaryGeneratedColumn()
+    id?: number;
+
+    @Column({ length: 200, nullable: true })
+    nom: string;
+
+    @Column({ length: 200, nullable: true })
+    prenom: string;
+
+    @Column({ length: 200, unique: true, nullable: true })
+    username: string;
+
+    @Column({ length: 200, nullable: true })
+    email: string;
+
+    @Column({ length: 200, nullable: true })
+    mot_de_passe: string;
+
+    @Column({ nullable: true })
+    date: Date;
+
+    @ManyToMany(() => TechniqueMeditation, (TechniqueForUser) => TechniqueForUser.id)
+    @JoinTable({ name: 'technique_user' }) technique_user: TechniqueMeditation[];
+
+}
